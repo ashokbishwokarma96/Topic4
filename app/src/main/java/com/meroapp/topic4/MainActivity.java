b73f6e2be2dea65cdbdc475e3da4d3405f2789fd
ashokbishwokarma96 <ashokbishwokarma96@gmail.com>
2019-10-25 10:55:14 +0545

    HashMap

--- app/src/main/java/com/meroapp/topic4/MainActivity.java
+++ app/src/main/java/com/meroapp/topic4/MainActivity.java
@@ -6,10 +6,21 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     private ListView listViewCountries;
-    public static final String countries[] = {"Nepal","India","China"};
+    public static final String countries[] = {
+
+            "Nepal","Kathmandu",
+            "India","New Dehli",
+            "China","Beijing"
+    };
+
+    private Map<String ,String> dictionary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +29,17 @@ public class MainActivity extends AppCompatActivity {
 
         listViewCountries = findViewById(R.id.listViewCountries);
 
+
+        dictionary = new HashMap<>();
+        for (int i=0;i<countries.length;i+=2){
+            dictionary.put(countries[i],countries[i+1]);
+        }
+
         ArrayAdapter countryAdapter = new ArrayAdapter<>(
 
                 this,
                 android.R.layout.simple_list_item_1,
-                countries
+                new ArrayList<String>(dictionary.keySet())
         );
         listViewCountries.setAdapter(countryAdapter);
 
