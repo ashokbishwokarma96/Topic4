3e83cb5173a4a739b44287b92cda42c8563e9aa1
ashokbishwokarma96 <ashokbishwokarma96@gmail.com>
2019-10-25 10:28:04 +0545

    list View

--- app/src/main/java/com/meroapp/topic4/MainActivity.java
+++ app/src/main/java/com/meroapp/topic4/MainActivity.java
@@ -3,12 +3,29 @@ package com.meroapp.topic4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewCountries;
+    public static final String countries[] = {"Nepal","India","China"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listViewCountries = findViewById(R.id.listViewCountries);
+
+        ArrayAdapter countryAdapter = new ArrayAdapter<>(
+
+                this,
+                android.R.layout.simple_list_item_1,
+                countries
+        );
+        listViewCountries.setAdapter(countryAdapter);
+
+
     }
 }
